70a7e2699cd407daa1a9d798148202b1e41a2ade
Jorge Dorio <jorgegabrielbd.jj@gmail.com>
2021-10-17 13:58:34 -0400

    update

--- scripts/script.js
+++ scripts/script.js
@@ -3,8 +3,8 @@ function mode() {
   modePic = document.getElementById('modePic').src
   atual = modePic.slice(-7)
   if (atual == 'lua.png') {
-    document.getElementById('modePic').src = 'sol.png'
-    mode.href = 'style_sobre_light.css'
+    document.getElementById('modePic').src = 'images/sol.png'
+    mode.href = 'style/style_sobre_light.css'
   } else {
   }
 }
